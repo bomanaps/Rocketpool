650b40cac91d5c591f5161b02519fddb1e8025eb
bomanaps <mercy.boma35@yahoo.com>
2023-06-08 01:58:02 +0100

    upgrade to mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,19 @@
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.0",
-   defaultNetwork: "sepolia",
+   defaultNetwork: "polygon_mumbai",
+   etherscan: {
+      apiKey: "FNWAP7AYYQWI7UE5QEK6GABF7GZT5X7JF8",
+    },
    networks: {
       hardhat: {},
-      sepolia: {
+      polygon_mumbai: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
